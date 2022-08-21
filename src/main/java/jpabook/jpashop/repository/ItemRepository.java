04f02fb43e2c8729877e757fd2c0731c52149f0a
KimWanJoo <wjwan0915@gmail.com>
2022-08-21 23:23:25 +0900

    :sparkles: 상품 Repository 구현

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager entityManager;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            entityManager.persist(item);
+            //id가 없으면 비어있는 것.
+        } else {
+            entityManager.merge(item);
+            //update 비슷한것.
+        }
+    }
+
+    public Item findOne(Long id) {
+        return entityManager.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return entityManager.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
